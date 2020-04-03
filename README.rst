1a1a8949b558402eece0f3d1e4928054b566329c
Josh Zuech <josh.zuech@nextthought.com>
2020-04-02 20:40:53 -0500

    Initial commit

---
+++ README.rst
@@ -0,0 +1,9 @@
+=============================
+ nti.site.license
+=============================
+
+.. image:: https://travis-ci.org/NextThought/nti.site.license.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.site.license
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.site.license/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.site.license?branch=master
---
+++ setup.py
@@ -0,0 +1,86 @@
+import codecs
+from setuptools import setup, find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'fudge',
+    'nti.dataserver[test]',
+    'nti.testing',
+    'zope.dottedname',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.site.license',
+    version=_read('version.txt').strip(),
+    author='Josh Zuech',
+    author_email='josh.zuech@nextthought.com',
+    description="NTI Site License",
+    long_description=(
+        _read('README.rst')
+        + '\n\n'
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='site license',
+    classifiers=[
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.site.license",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.site'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'BTrees',
+        'nti.containers',
+        'nti.dublincore',
+        'nti.externalization',
+        'nti.mimetype',
+        'nti.property',
+        'nti.schema',
+        'nti.wref',
+        'nti.zope_catalog',
+        'setuptools',
+        'six',
+        'ZODB',
+        'zope.annotation',
+        'zope.component',
+        'zope.container',
+        'zope.event',
+        'zope.location',
+        'zope.interface',
+        'zope.intid',
+        'zope.security',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
